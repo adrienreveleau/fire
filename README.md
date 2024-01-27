63a0619a30ece6eb0a929742f58bffd60a759ea0
adrienreveleau <reveleauadrien@gmail.com>
2024-01-27 02:29:13 +0100

    Améliorations portées sur le déboggage et le système d'observation des fichiers

--- README.md
+++ README.md
@@ -14,14 +14,19 @@ $ pip install watchdog
 $ git clone https://github.com/adrienreveleau/fire.git
 ```
 ```bash
-$ python fire.py <builder> <build dest> <exec command> 
+$ python fire.py <compiler> <builder> <build dest> <exec command> 
 ```
-
+*Pour que votre fichier puisse être exécutable par **fire** il vous faudra exécuter la commande suivante :*
+```bash
+$ chmod +x chemin/vers/votre/fichier.sh
+```
 #### Variables
 
 ```python
-FORMATS = [".js", ".h", ".cpp", ".css", ".scss", ".html", ".ts", ".go"]
-BUILD_CMD = sys.argv[1] # "ninja"
-BUILD_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), sys.argv[2])
-START_CMD = f"{BUILD_DIR}/{sys.argv[3]}" 
+CONFIG = {"logger": False, "logLVL": 0} # ex. --LOGGER=False --LOGLVL=0
+FORMATS = [".js", ".h", ".cpp", ".css", ".scss", ".html", ".ts"]
+BUILD_CMD = sys.argv[2] # ex. "ninja"
+BUILD_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), sys.argv[3]) # ex. ../build
+START_CMD = f"{BUILD_DIR}/{sys.argv[4]}" # ex. htmlc 
+COMPILE_CMD = sys.argv[1] # ex. ../compile.sh
 ```
--- fire.py
+++ fire.py
@@ -1,11 +1,13 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-import os, time, subprocess, sys
+import os, time, subprocess, sys, shlex
 
-FORMATS = [".js", ".h", ".cpp", ".css", ".scss", ".html", ".ts", ".go"]
-BUILD_CMD = sys.argv[1] # "ninja"
-BUILD_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), sys.argv[2])
-START_CMD = f"{BUILD_DIR}/{sys.argv[3]}" 
+CONFIG = {"logger": False, "logLVL": 0} # ex. --LOGGER=False --LOGLVL=0
+FORMATS = [".js", ".h", ".cpp", ".css", ".scss", ".html", ".ts"]
+BUILD_CMD = sys.argv[2] # ex. "ninja"
+BUILD_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), sys.argv[3]) # ex. ../build
+START_CMD = f"{BUILD_DIR}/{sys.argv[4]}" # ex. htmlc 
+COMPILE_CMD = sys.argv[1] # ex. ../compile.sh
 
 print("""
                                       
@@ -40,21 +42,53 @@ print("""
 print("fire v0.1")
 print(" ")
 print("----------- Configuration ------------")
-print(f"Commande de compilation : {BUILD_CMD}")
-print(f"Dossier de compilation : {BUILD_DIR}")
-print(f"Commande d'exécution : {START_CMD}")
+print(f"Commande de Compilation :   {COMPILE_CMD}")
+print(f"Commande de Build       :   {BUILD_CMD}")
+print(f"Dossier de Compilation  :   {BUILD_DIR}")
+print(f"Commande d'exécution    :   {START_CMD}")
+print(f"logs                    :   {CONFIG['logger']}")
+print(f"Niveau de logs          :   {CONFIG['logLVL']}")
 print("------------------------------------- ")
 print(" ")
 print("Nous sommes prêts à compiler vos scandaleux fichiers.")
 print(" ")
 
-def compile():
-    print("Journal de compilation :")
-    subprocess.run(BUILD_CMD, shell=True, cwd=BUILD_DIR)
-    print("Compilation terminée avec succès.")
+def formatCMD(c):
+    return shlex.split(c)
+
+def letdrain(f):
+    if not os.listdir(BUILD_DIR):
+        logger("Dossier de build vierge.", 1)
+        return False
+    else:
+        logger("Preparation du dossier de build...", 1)
+        for file in os.listdir(f):
+            path = os.path.join(f, file)
+            if os.path.isfile(path):
+                os.remove(path)
+    print("Votre dossier est prét !", 1)
+    return True
+
+def letexec():
     print("\nExécution de l'application...")
     subprocess.run(START_CMD, shell=True, cwd=BUILD_DIR)
-    print("Exécution terminée avec succès.\n")
+    print("Exécuté avec succès.\n")
+
+def letcompile():
+    logger("Journal de Compilation :", 1)
+    subprocess.run(COMPILE_CMD, shell=True, cwd=BUILD_DIR)
+    print("Compilation terminée avec succès.")
+
+def letbuild():
+    logger("Journal de build :", 1)
+    subprocess.run(BUILD_CMD, shell=True, cwd=BUILD_DIR)
+    print("Build terminé avec succès.")
+
+def logger(l, lvl):
+    if CONFIG['logger'] and CONFIG['logLVL'] == lvl:
+        print(l)
+    else:
+        return
 
 class FileHandler(FileSystemEventHandler):
     def on_modified(self, event):
@@ -63,11 +97,21 @@ class FileHandler(FileSystemEventHandler):
         self.process(event)
     def on_deleted(self, event):
         self.process(event)
-
     def process(self, event):
+        logger(" ", 2) 
+        logger(f"Format ok ?        :   {any(event.src_path.endswith(f) for f in FORMATS)}", 2)
+        logger(f"Src                :   {event.src_path}", 2)
+        logger(f"Build dir          :   {os.path.abspath(BUILD_DIR)}", 2)
+        logger(f"Fichier build ?    :   {event.src_path.startswith(os.path.abspath(BUILD_DIR))}", 2)
+        if event.src_path.startswith(os.path.abspath(BUILD_DIR)):
+            return
         if  any(event.src_path.endswith(f) for f in FORMATS):
-            print("\nDes modifications ont été détectées. Compilation de l'application en cours...")
-            compile()
+            time.sleep(5)
+            letdrain(BUILD_DIR)
+            letcompile()
+            letbuild()
+            letexec()
+            return
 
 if __name__ == '__main__':
     event_handler = FileHandler()
@@ -77,8 +121,12 @@ if __name__ == '__main__':
         path=".",
         recursive=True,
     )
+
+    letdrain(BUILD_DIR)
     observer.start()
-    compile()
+    letcompile()
+    letbuild()
+    letexec()
 
     try:
         while True:
